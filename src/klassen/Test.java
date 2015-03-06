5858773305d23b166b1de872aab026d7d4dddf22
lukas <resch.lukas10@gmail.com>
2015-03-06 13:32:38 -0800

    Hallo!

--- src/klassen/Test.java
+++ src/klassen/Test.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package klassen;
 
-/**
- *
- * @author Christian
- */
 public class Test
 {
   public static void main(String[] args)
   {
     
+      System.out.println("Hallo");
+      
   }
 }
